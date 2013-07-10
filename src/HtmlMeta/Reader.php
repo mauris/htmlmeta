7c811be06374b7f3258334a88e0e9d9025914184
Sam-Mauris Yong <mauris@hotmail.sg>
2013-07-10 22:06:43 +0800

    reader class implementation

---
+++ src/HtmlMeta/Reader.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace HtmlMeta;
+
+use Symfony\Component\DomCrawler\Crawler;
+
+class Reader implements ReaderInterface
+{
+    public function parse($html)
+    {
+        $result = array();
+
+        $crawler = new Crawler($html);
+        $crawler = $crawler->filterXPath('//meta');
+        foreach($crawler as $node){
+            $attributes = array();
+            $nodeAttributes = $node->attributes;
+            $length = $nodeAttributes->length;
+            for ($i = 0; $i < $length; ++$i) {
+                $attributes[$nodeAttributes->item($i)->name] = $nodeAttributes->item($i)->value;
+            }
+            $result[] = new Meta($attributes);
+        }
+
+        return $result;
+    }
+}
